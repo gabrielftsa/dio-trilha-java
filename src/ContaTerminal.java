d67130d44a09103010c83e0c0772635af550eb5a
gabrielftsa <gabrielftsa@gmail.com>
2023-06-21 17:49:35 -0300

    adiciona atividade

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) {
-            
+
+        Scanner leitor = new Scanner(System.in);
+
+        int numero;
+        String Agencia = "067-8";
+        String nomeCliente;
+        double saldo = 0d;
+
+        System.out.println("Bem-vindo(a) à Agência nº 067-8\n ");
+        System.out.println("Por favor, insira seu novo número identificador da Agência !");
+        numero = leitor.nextInt();
+
+        System.out.println("Por favor, insira seu nome completo !");
+        nomeCliente = leitor.next();
+
+        System.out.println("Por favor, insira seu saldo.");
+        saldo = leitor.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco," +
+                " sua agência é " + Agencia +", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
     }
 }
